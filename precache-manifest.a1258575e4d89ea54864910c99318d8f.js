f7161a50eac98a14979d486c691be175912b0854
Sanket Kumar Singh <sanketkumarsingh@iitkgp.ac.in>
2019-10-12 01:29:50 +0530

    Updates

---
+++ precache-manifest.a1258575e4d89ea54864910c99318d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c412fb144f321b5f2f027c747d8a7d",
+    "url": "/myapps/index.html"
+  },
+  {
+    "revision": "22b179574b457f294e72",
+    "url": "/myapps/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8cd9bf2de5a97d07f0e1",
+    "url": "/myapps/static/css/main.14c14f69.chunk.css"
+  },
+  {
+    "revision": "22b179574b457f294e72",
+    "url": "/myapps/static/js/2.267855e8.chunk.js"
+  },
+  {
+    "revision": "8cd9bf2de5a97d07f0e1",
+    "url": "/myapps/static/js/main.bbac1bbc.chunk.js"
+  },
+  {
+    "revision": "82cd7a9fbb586e382e3e",
+    "url": "/myapps/static/js/runtime-main.79bd60dc.js"
+  }
+]);
